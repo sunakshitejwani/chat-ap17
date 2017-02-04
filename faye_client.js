520947b01d78ec45d18c9d1782945834908acf1b
sunakshi96 <sunakshitejwani@gmail.com>
2017-02-04 23:46:40 +0530

    make connection

--- faye_client.js
+++ faye_client.js
@@ -1,10 +1,20 @@
-var faye = require('faye');
+/*var Faye = require('faye');
 
-var client1= new faye.Client('http://localhost:8001/faye');
+var client1= new Faye.Client('http://localhost:8001/faye');
 var pub  = client1.publish('/messages',{ text: "Hello World!"});
 pub.then(function(){
 	alert("Message received by the server!");
 },function(error){
 	alert("There was a problem: "+error.message);
 })
+pub.callback(process.exit);*/
+
+var Faye   = require('faye');
+
+var client = new Faye.Client('http://localhost:8001/faye');
+
+var pub = client.publish('/messages', {
+  text: 'Hello world'
+});
+
 pub.callback(process.exit);
--- faye_client2.js
+++ faye_client2.js
@@ -1,11 +1,17 @@
-var faye = require('faye');
+/*var Faye = require('faye');
 
-var client1= new faye.Client('http://localhost:8001/faye');
+var client1= new Faye.Client('http://localhost:8001/faye');
 client1.connect();
 
 var subscription=client1.subscribe('/messages',function(message){
 
-	console.log("Got a message",message.text);
+	console.log("Got a message ",message.text);
 
+});*/
+var Faye   = require('faye');
+
+var client = new Faye.Client('http://localhost:8001/faye');
+
+client.subscribe('/messages', function(message) {
+  console.log('Got a message: ' + message.text);
 });
- 
